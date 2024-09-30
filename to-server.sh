6749865857460fd52d5ea93551e44301b8a81fb7
maybetree <maybetree48@proton.me>
2024-09-30 15:15:36 +0200

    Web design

--- to-server.sh
+++ to-server.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-rsync -avz $(git ls-files) bbox-local:/root/srv/bardak
-ssh -t bbox-local "cd /root/srv/bardak; docker-compose down -t 1; docker-compose up --build -d"
+rsync -avz $(git ls-files) bbox.home:/root/srv/bardak
+ssh -t bbox.home "cd /root/srv/bardak; docker-compose down -t 1; docker-compose up --build -d"
 
